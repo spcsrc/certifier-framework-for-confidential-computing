bfa0d3863523b02d5c621c4113abb3375e911b6a
Anoop Jaishankar <ajaishankar@vmware.com>
2023-02-22 00:42:57 -0800

    Update Gramine verify evidence with measuremnets

--- certifier_service/gramineverify/gramineverify.go
+++ certifier_service/gramineverify/gramineverify.go
@@ -35,10 +35,10 @@ import (
 )
 
 func GramineVerifyEvidence(evidence []byte, endorsements []byte) ([]byte, []byte, error) {
-	var size_bytes = make([]byte, 4)
-	copy (size_bytes, evidence)
+	var assertion_size_bytes = make([]byte, 4)
+	copy (assertion_size_bytes, evidence)
 
-	assertion_size := binary.LittleEndian.Uint32(size_bytes)
+	assertion_size := binary.LittleEndian.Uint32(assertion_size_bytes)
 	fmt.Printf("GramineVerifyEvidence assertion_size: %v\n", assertion_size)
 
 	var assertion = make([]byte, assertion_size)
@@ -46,18 +46,25 @@ func GramineVerifyEvidence(evidence []byte, endorsements []byte) ([]byte, []byte
 
 	evidencePtr := C.CBytes(assertion)
 	defer C.free(evidencePtr)
+
+	var user_data_size_bytes = make([]byte, 4)
+	user_data_size_bytes = evidence[4 + assertion_size:8 + assertion_size]
+	user_data_size := binary.LittleEndian.Uint32(user_data_size_bytes)
+	fmt.Printf("GramineVerifyEvidence user_data_size: %v\n", user_data_size)
+
+	var user_data = make([]byte, user_data_size)
+	user_data = evidence[8 + assertion_size:8 + assertion_size + user_data_size]
+
 	endorsementsPtr := C.CBytes(endorsements)
 	defer C.free(endorsementsPtr)
 
-	customClaimOutSize := C.ulong(4096)
-	customClaimOut := C.malloc(customClaimOutSize)
-	defer C.free(unsafe.Pointer(customClaimOut))
-	measurementSize := C.ulong(256)
+	measurementSize := C.ulong(32)
 	measurementOut := C.malloc(measurementSize)
 	defer C.free(unsafe.Pointer(measurementOut))
 
-	ret := C.graminelib_verify_quote((*C.uchar)(evidencePtr),
-		C.ulong(assertion_size))
+	ret := C.graminelib_verify_quote(C.ulong(assertion_size),
+		(*C.uchar)(evidencePtr), (*C.ulong)(&measurementSize),
+		(*C.uchar)(measurementOut))
 
 	if ret != 0 {
 		return nil, nil, fmt.Errorf("graminelib_verify_quote failed");
@@ -69,11 +76,9 @@ func GramineVerifyEvidence(evidence []byte, endorsements []byte) ([]byte, []byte
 	C.myPrintFunction(str)
 	C.free(unsafe.Pointer(str))
 
-	outCustomClaims := C.GoBytes(unsafe.Pointer(customClaimOut),
-		C.int(customClaimOutSize))
 	outMeasurement := C.GoBytes(unsafe.Pointer(measurementOut),
 		C.int(measurementSize))
 
 	fmt.Printf("GramineVerifyEvidence done\n")
-	return outCustomClaims, outMeasurement, nil
+	return user_data, outMeasurement, nil
 }
